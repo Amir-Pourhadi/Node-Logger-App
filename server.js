cf01b7cb48b70db532d55886e4d16b7cdb76cda4
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-22 20:40:36 +0330

    Use morgan

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const morgan = require("morgan");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use morgan to log requests to the console
+app.use(morgan(":method :status :url 'HTTP/:http-version'"));
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Morgan Logger App!");
+});
+
 // Listen on the port and Set up the logger
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
