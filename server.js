4ffc64d425ae79d537ae2f09a31c0f74e3dc7c59
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-22 21:00:19 +0330

    Use morgan format and token methods

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const { v4: uuidv4 } = require("uuid");
 
 // Initialize the app and set the port
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Add morgan token to each request
+morgan.token("id", (req) => req.id);
+
+// Set a random uuid to each request
+app.use((req, res, next) => {
+  req.id = uuidv4();
+  next();
+});
+
+// Use a Sample morgan token
+morgan.token("param", (req, res, param) => "userToken");
+
 // Use morgan to log requests to the console
-app.use(morgan(":method :status :url 'HTTP/:http-version'"));
+morgan.format("myFormat", ":id :method :url :status :response-time ms :param");
+app.use(morgan('myFormat'));
 
 // Home route
 app.get("/", (req, res) => {
