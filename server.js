81245ea12ac66cfc412ca6286a9dbbadd6625d84
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-22 21:56:20 +0330

    Stream logs to a file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
 // Initialize the app and set the port
 const app = express();
@@ -18,9 +20,18 @@ app.use((req, res, next) => {
 // Use a Sample morgan token
 morgan.token("param", (req, res, param) => "userToken");
 
-// Use morgan to log requests to the console
+// Use stream to write morgan's logs to a file instead of the console
+// "a" flag to append to the file instead of overwriting it
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+
+// Set morgan format
 morgan.format("myFormat", ":id :method :url :status :response-time ms :param");
-app.use(morgan('myFormat'));
+
+// Use morgan to log requests to the console
+app.use(morgan("myFormat"));
+
+// Use morgan to log requests to the access.log file
+app.use(morgan("myFormat", { stream: accessLogStream }));
 
 // Home route
 app.get("/", (req, res) => {
